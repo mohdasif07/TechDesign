cadc34bd23e6c5584eff3e15b9fdb0849021fd5f
@mohd.asif1 <mohd.asif@techqware.com>
2026-08-27 10:05:26 +0530

    intial commit

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source "https://rubygems.org"
+
+gem "rails", "~> 8.0"
+gem "propshaft"
+gem "importmap-rails"
+
+gem "puma", "~> 8.0"
+
+gem "sqlite3", "~> 2.9"
---
+++ config/application.rb
@@ -0,0 +1,21 @@
+require_relative "boot"
+
+require "rails"
+require "active_model/railtie"
+require "active_job/railtie"
+require "action_controller/railtie"
+require "action_view/railtie"
+require "action_mailer/railtie"
+require "action_cable/engine"
+require "active_storage/engine"
+require "rails/test_unit/railtie"
+
+
+Bundler.require(*Rails.groups)
+
+module AsDesigntech
+  class Application < Rails::Application
+    config.load_defaults 8.0
+    config.autoload_lib(ignore: %w[assets tasks])
+  end
+end
