aa26605f7089d154a6961eadd659cb9930bb72f2
@mohd.asif1 <mohd.asif@techqware.com>
2026-09-12 20:08:36 +0530

    Seo changes

--- app/controllers/sitemap_controller.rb
+++ app/controllers/sitemap_controller.rb
@@ -1,6 +1,15 @@
 class SitemapController < ApplicationController
   def index
     @urls = SitemapBuilder.urls
-    render layout: false, formats: [:xml]
+    xml = render_to_string(template: "sitemap/index", formats: [:xml], layout: false)
+
+    response.headers["Content-Type"] = "application/xml; charset=utf-8"
+    response.headers["Cache-Control"] = "public, max-age=3600, must-revalidate"
+    response.headers["Content-Length"] = xml.bytesize.to_s
+
+    # Return empty body on HEAD but keep the real Content-Length so fetchers
+    # (including Google) do not treat the sitemap as empty.
+    self.status = 200
+    self.response_body = request.head? ? [""] : [xml]
   end
 end
